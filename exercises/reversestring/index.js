330ab8308cfc5d0bcbf330b2f481befbc67b79ad
Mohammed Chisti <93mchisti@gmail.com>
2019-11-06 14:30:36 -0500

    All three solutions

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,39 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+
+// Solution 1
+// Takeaway: Uses built-in JavaScript reverse function.
+// function reverse(str) {
+//   return str.split('').reverse().join('')
+// }
+
+// Solution 2
+// Takeaway: Removes the use of JavaScript's built-in reverse function for more manual reverse.
+// function reverse(str) {
+//   let reversed = ''
+//
+//   // Loop 1: For loop solutions.
+//   // let letters = str.split('')
+//   //
+//   // for(n = letters.length-1; n > -1; n--) {
+//   //   reversed += letters[n]
+//   // }
+//
+//   // Loop 2: Simpliar for loop.
+//   for(let letter of str) {
+//     reversed = letter + reversed
+//   }
+//
+//   return reversed
+// }
+
+// Solution 3
+// Takeaway: Using JavaScript's reduce function to show dominance.
+function reverse(str) {
+  return str.split('').reduce((reversed, char) => char + reversed, '')
+}
+
+reverse("Hello")
 
 module.exports = reverse;
