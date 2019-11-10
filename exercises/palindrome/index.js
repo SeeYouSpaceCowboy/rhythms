321095e6aaa05744684c4e33a32561535aacbcc6
Mohammed Chisti <93mchisti@gmail.com>
2019-11-10 15:18:57 -0500

    .

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,6 +7,20 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+// Solution 1
+// Takeaway: Most straightforward answer
+// function palindrome(str) {
+//   const reversed = str.split('').reverse().join('')
+//
+//   return str === reversed
+// }
+
+// Solution 2
+// Takeaway:
+function palindrome(str) {
+
+
+  return str
+}
 
 module.exports = palindrome;
